51ebef860ae39a778e65ee2764205c0abe218459
Dakshet <dakshghole@gmail.com>
2024-05-02 18:19:31 +0530

    Minimum length set

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -50,7 +50,7 @@ const Signup = ({ showAlert }) => {
             <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" name='name' onChange={onChange} />
+                    <input type="text" className="form-control" id="name" name='name' onChange={onChange} minLength={3} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email</label>
@@ -58,11 +58,11 @@ const Signup = ({ showAlert }) => {
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" name="password" onChange={onChange} />
+                    <input type="password" className="form-control" id="password" name="password" onChange={onChange} minLength={4} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="cpassword" className="form-label">Confirm Password</label>
-                    <input type="password" className="form-control" id="cpassword" name="cpassword" onChange={onChange} />
+                    <input type="password" className="form-control" id="cpassword" name="cpassword" onChange={onChange} minLength={4} />
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
